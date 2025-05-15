2704f3dcd5bd4b9cc0928fbd09c5b56d66704d3f
Ionutmnt14 <ionutmuntean350@gmail.com>
2025-05-15 11:39:49 +0300

    Bar, area and pie charts integrated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import AppAreaChart from "@/components/AppAreaChart";
+import AppPieChart from "@/components/AppPieChart";
+import AppBarChart from "@/components/BarChart";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4 ">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">text</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">text</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">text</div>
+    </div>
+  );
 };
 
 export default page;
